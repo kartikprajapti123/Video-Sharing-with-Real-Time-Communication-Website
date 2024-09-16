14e23e22f14951ea52aa66cb62792016b7469ae4
kartikprajapti123 <kartikprajapati26122004@gmail.com>
2024-09-16 09:15:59 +0530

    Update settings.py

--- jerry_project/settings.py
+++ jerry_project/settings.py
@@ -141,7 +141,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
