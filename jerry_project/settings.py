2f92357172032476acbeaad2301f049baac8133f
kartikprajapti123 <155372801+kartikprajapti123@users.noreply.github.com>
2024-11-09 10:23:46 +0530

    Update settings.py

--- jerry_project/settings.py
+++ jerry_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','wwww.shareyourclips.online','shareyourclips.online']
 
 # Application definition
 INSTALLED_APPS = [
